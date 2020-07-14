840e66b47733a9bc3dce0c1ec4dd8eb9234b5ea5
stat-ki <per_ik@outlook.com>
2020-07-14 15:12:52 +0900

    Finish 1-1

---
+++ Chapter_1/Exercise_1/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter1_1
+{
+    public class Product
+    {
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Product(int code, string name, int price)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+
+        public int GetTaxIncludingPrice()
+        {
+            return Price + GetTax();
+        }
+    }
+}
---
+++ Chapter_1/Exercise_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Chapter1_1;
+
+namespace Exercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product dorayaki = new Product(98, "どら焼き", 210);
+            int tax = dorayaki.GetTax();
+            System.Console.WriteLine($"{tax}円");
+        }
+    }
+}
