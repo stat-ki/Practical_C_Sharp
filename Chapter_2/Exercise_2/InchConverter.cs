5dd4545866844f15194267577d1142c2464b7da5
stat-ki <per_ik@outlook.com>
2020-07-15 13:21:16 +0900

    Finish 2-2

---
+++ Chapter_2/Exercise_2/InchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    public static class InchConverter
+    {
+        private const double ratio = 0.0254;
+
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+
+        public static double FromInch(double inch)
+        {
+            return inch * ratio;
+        }
+    }
+}
---
+++ Chapter_2/Exercise_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                PrintInchToMeter(1, 10);
+            }
+            else
+            {
+                PrintMeterToInch(1, 10);
+            }
+        }
+
+        static void PrintInchToMeter(int start, int stop)
+        {
+            for (int inch = start; inch <= stop; inch++)
+            {
+                double meter = InchConverter.FromInch(inch);
+                Console.WriteLine("{0} inch = {1:0.000} m", inch, meter);
+            }
+        }
+
+        static void PrintMeterToInch(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} meter = {1:0.000} inch", meter, inch);
+            }
+        }
+    }
+}
