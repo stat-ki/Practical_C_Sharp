4bea5fe4954d53a7cb05a67da6b0d1e1918836ed
stat-ki <per_ik@outlook.com>
2020-07-15 13:27:13 +0900

    Finish 2-3

---
+++ Chapter_2/Exercise_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SalesCaluculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sales = new SalesCounter("../../../sales.csv");
+            IDictionary<string, int> amoutPerProductCategory = sales.GetProductCategorySales();
+            foreach (var obj in amoutPerProductCategory) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+
+        
+    }
+}
--- Chapter_2/Exercise_3/SalesCounter.cs
+++ Chapter_2/Exercise_3/SalesCounter.cs
@@ -1,10 +1,47 @@
 ﻿using System;
-namespace Exercise_3
+using System.Collections.Generic;
+using System.IO;
+
+namespace SalesCaluculator
 {
     public class SalesCounter
     {
-        public SalesCounter()
+        private IEnumerable<Sale> _sales;
+
+        public SalesCounter(string filePath)
         {
+            _sales = ReadSales(filePath);
+        }
+
+        public IDictionary<string, int> GetProductCategorySales()
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales)
+            {
+                if (dict.ContainsKey(sale.ProductCategory))
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    dict[sale.ProductCategory] = sale.Amount;
+            }
+            return dict;
+        }
+
+        private static IEnumerable<Sale> ReadSales(string filePath)
+        {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(",");
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
