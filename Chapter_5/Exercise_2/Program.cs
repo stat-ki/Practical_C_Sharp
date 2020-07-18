ad68ecf4514eb5d5b9880d931a3ff2814a0557e2
stat-ki <per_ik@outlook.com>
2020-07-18 11:43:10 +0900

    Finish 5-2

---
+++ Chapter_5/Exercise_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            int num;
+            if (int.TryParse(s, out num))
+            {
+                Console.WriteLine("{0:#,#}", num);
+            }
+            else
+            {
+                Console.WriteLine("not number");
+            }
+        }
+    }
+}
