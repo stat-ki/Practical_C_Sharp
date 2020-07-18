e0d3ddbd66333d2f50e204e5389f035624d7353d
stat-ki <per_ik@outlook.com>
2020-07-18 11:36:01 +0900

    Finish 5-1

---
+++ Chapter_5/Exercise_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase: true) == 0)
+            {
+                Console.WriteLine("equal");
+            }
+            else
+            {
+                Console.WriteLine("not equal");
+            }
+        }
+    }
+}
