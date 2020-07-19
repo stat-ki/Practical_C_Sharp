069e0c5c289928547461d5ea5f60f9a839ab2864
stat-ki <per_ik@outlook.com>
2020-07-19 14:47:32 +0900

    Finish 6-1

---
+++ Chapter_6/Exercise_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            var max_number = numbers.Max();
+            Console.WriteLine(max_number);
+            Console.WriteLine("----");
+
+            var skip = numbers.Length - 2;
+            foreach (var n in numbers.Skip(skip))
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("----");
+
+            var strs = numbers.Select(n => n.ToString());
+            foreach(var s in strs)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("----");
+
+            var sorted = numbers.OrderBy(n => n);
+            foreach(var i in sorted.Take(3))
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("----");
+
+            var count = numbers.Distinct().Count(n => n >= 10);
+            Console.WriteLine(count);
+        }
+    }
+}
