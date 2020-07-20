a981cca2892902e0835bf23fc3c87a358137d530
stat-ki <per_ik@outlook.com>
2020-07-20 13:41:24 +0900

    Finish 7-2

---
+++ Chapter_7/Exercise_2/Abbreviations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Exercise_2
+{
+    class Abbreviations : IEnumerable<KeyValuePair<string, string>>
+    {
+        private Dictionary<string, string> _dict = new Dictionary<string, string>();
+
+        public Abbreviations()
+        {
+            var lines = File.ReadAllLines("../../../Abbreviations.txt");
+            _dict = lines.Select(line => line.Split('='))
+                         .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        public void Add(string abbr, string japanese)
+        {
+            _dict[abbr] = japanese;
+        }
+
+        public string this[string abbr]
+        {
+            get
+            {
+                return _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+            }
+        }
+
+        public string ToAbbreviation(string japanese)
+        {
+            return _dict.FirstOrDefault(x => x.Value == japanese).Key;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
+        {
+            foreach (var item in _dict)
+            {
+                if (item.Value.Contains(substring))
+                    yield return item;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _dict.Count;
+            }
+        }
+
+        public bool Remove(string abb)
+        {
+            return _dict.Remove(abb);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, string>>)_dict).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, string>>)_dict).GetEnumerator();
+        }
+    }
+}
---
+++ Chapter_7/Exercise_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abbrs = new Abbreviations();
+
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核拡散防止条約");
+
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine("----");
+
+            if (abbrs.Remove("NPT"))
+            {
+                Console.WriteLine(abbrs.Count);
+            }
+            else
+            {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine("----");
+
+            foreach (var item in abbrs.Where(x => x.Key.Length == 3))
+            {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
+        }
+    }
+}
