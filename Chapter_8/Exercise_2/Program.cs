72daaedb45d63a33854739a34556aaf1d2a7cccc
stat-ki <per_ik@outlook.com>
2020-07-21 20:41:06 +0900

    Finish 8-2

---
+++ Chapter_8/Exercise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt = DateTime.Today;
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine("{0:yy/MM/dd} next {1} is {2:yy/MM/dd}", dt, (DayOfWeek)dayofweek, NextDay(dt, (DayOfWeek)dayofweek));
+            }
+        }
+
+        public static DateTime NextDay(DateTime date, DayOfWeek dayOfWeek)
+        {
+            var nextweek = date.AddDays(7);
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            return nextweek.AddDays(days);
+        }
+    }
+}
