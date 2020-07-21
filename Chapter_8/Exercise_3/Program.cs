8ed545a8d89e1a53aeefd7591b220c31463bfa6b
stat-ki <per_ik@outlook.com>
2020-07-21 20:50:58 +0900

    Finish 8-3

---
+++ Chapter_8/Exercise_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tw = new TimeWatch();
+            tw.Start();
+            for (var i = 1; i <= 100; i ++)
+            {
+                Console.WriteLine(i);
+            }
+            TimeSpan duration = tw.Stop();
+            Console.WriteLine("run time was {0}s", duration.TotalMilliseconds);
+        }
+    }
+
+    class TimeWatch
+    {
+        private DateTime _time;
+
+        public void Start()
+        {
+            _time = DateTime.Now;
+        }
+
+        public TimeSpan Stop()
+        {
+            return DateTime.Now - _time;
+        }
+    }
+}
