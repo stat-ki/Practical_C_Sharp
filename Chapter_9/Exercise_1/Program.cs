f389f43ce791cefaf9db86afbfe5ea192ea70db7
stat-ki <per_ik@outlook.com>
2020-07-22 22:10:10 +0900

    Finish 9-1

---
+++ Chapter_9/Exercise_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+
+            var count = 0;
+            using (var sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line.Contains(" class "))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+            Console.WriteLine("----");
+
+            //File.ReadAlllinesメソッドを使うパターン
+            //var count = File.ReadAllLines(file).Count(s => s.Contains(" class "));
+            //Console.WriteLine(count);
+            //Console.WriteLine("----");
+
+            //File.ReadLinesメソッドを使うパターン
+            //var count = File.ReadLines(file).Count(s => s.Contains(" class "));
+            //Console.WriteLine(count);
+            //Console.WriteLine("----");
+        }
+    }
+}
