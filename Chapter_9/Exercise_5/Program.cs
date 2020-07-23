6b8eb8e68af7906a74355ed707fd1ed6e0b7077d
stat-ki <per_ik@outlook.com>
2020-07-23 13:55:36 +0900

    Finish 9-5

---
+++ Chapter_9/Exercise_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = args[0];
+            DisplayLargeFile(dir, 1 * 1024L * 1024L);
+        }
+
+        private static void DisplayLargeFile(string dir, long size)
+        {
+            var files = Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories)
+                                 .Where(file => FileSize(file) >= size);
+            foreach (var file in files)
+                Console.WriteLine(file);
+        }
+
+        private static long FileSize(string file)
+        {
+            var fi = new FileInfo(file);
+            return fi.Length;
+        }
+    }
+}
