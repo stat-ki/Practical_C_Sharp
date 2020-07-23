5e936e141256c4a672dba7ea9bb5677f58c043e8
stat-ki <per_ik@outlook.com>
2020-07-23 13:42:05 +0900

    Finish 9-2

---
+++ Chapter_9/Exercise_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+            var lines = File.ReadLines(file).Select((s, n) => string.Format("{0,4}: {1}", n + 1, s));
+            var path = Path.ChangeExtension(file, ".txt");
+            File.WriteAllLines(path, lines);
+            var text = File.ReadAllText(path);
+            Console.WriteLine(text);
+
+        }
+    }
+}
