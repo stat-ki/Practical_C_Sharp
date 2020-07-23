f46ada5d670bc37f8855865a10ef4e30b2342305
stat-ki <per_ik@outlook.com>
2020-07-23 13:52:44 +0900

    Finish 9-4

---
+++ Chapter_9/EXercise_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace EXercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceDir = args[0];
+            var destDir = args[1];
+            CopyFiles(sourceDir, destDir);
+        }
+
+        private static void CopyFiles(string sourceDir, string destDir)
+        {
+            var files = Directory.EnumerateFiles(sourceDir, "*.*");
+            if (!Directory.Exists(destDir))
+                Directory.CreateDirectory(destDir);
+            foreach (var file in files)
+            {
+                var dest = GetBakFilePath(destDir, file);
+                Console.WriteLine(dest);
+                File.Copy(file, dest, overwrite: true);
+            }
+        }
+
+        private static string GetBakFilePath(string destDir, string file)
+        {
+            var name = Path.GetFileNameWithoutExtension(file) + "_bak";
+            var ext = Path.GetExtension(file);
+            return Path.Combine(destDir, name + ext);
+        }
+    }
+}
