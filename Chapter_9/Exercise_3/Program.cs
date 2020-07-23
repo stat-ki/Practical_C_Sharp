83811fd5a34601d86387180df8b01a866a3fafad
stat-ki <per_ik@outlook.com>
2020-07-23 13:45:56 +0900

    Finish 9-3

---
+++ Chapter_9/Exercise_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file1 = args[0];
+            var file2 = args[1];
+            File.AppendAllLines(file1, File.ReadLines(file2));
+        }
+    }
+}
