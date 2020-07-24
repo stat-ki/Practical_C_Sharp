e2b7963978896bda2e10848f2b877341389fab7b
stat-ki <per_ik@outlook.com>
2020-07-24 17:19:09 +0900

    Finish 10-2

---
+++ Chapter_10/Exercise_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+            PickNumber(file);
+        }
+
+        private static void PickNumber(string file)
+        {
+            foreach (var line in File.ReadLines(file))
+            {
+                var matches = Regex.Matches(line, @"\b\d{3,}\b");
+                foreach (Match m in matches)
+                {
+                    Console.WriteLine(m.Value);
+                }
+            }
+        }
+    }
+}
