337dd3423eb9e301032b9b79af415517eda0ec59
stat-ki <per_ik@outlook.com>
2020-07-24 17:11:25 +0900

    Finish 10-1

---
+++ Chapter_10/Exercise_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPhoneNumber("080-1234-5678"));
+            Console.WriteLine(IsPhoneNumber("123-4567-8901"));
+        }
+
+        static bool IsPhoneNumber (string str)
+        {
+            return Regex.IsMatch(str, @"^0[789]0-\d{4}-\d{4}$");
+        }
+    }
+}
