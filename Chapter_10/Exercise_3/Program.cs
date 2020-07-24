791f74b8e69110a7cc2db1d480e3fe792ebacc44
stat-ki <per_ik@outlook.com>
2020-07-24 17:40:56 +0900

    Finish 10-3

---
+++ Chapter_10/Exercise_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+
+            foreach (var line in texts)
+            {
+                var matches = Regex.Matches(line, @"\btime\b", RegexOptions.IgnoreCase);
+                foreach (Match m in matches)
+                {
+                    Console.WriteLine(m.Index);
+                }
+            }
+        }
+    }
+}
