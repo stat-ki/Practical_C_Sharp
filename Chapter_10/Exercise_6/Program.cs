e232293c876c6117c0a85d5c1c7a6445d7a9c9e6
stat-ki <per_ik@outlook.com>
2020-07-24 18:09:33 +0900

    Finish 10-6

---
+++ Chapter_10/Exercise_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ";
+            var pattern = @"\b(\w)(\w)\w\2\1\b";
+            var matches = Regex.Matches(text, pattern);
+            foreach (Match m in matches)
+            {
+                Console.WriteLine("'{0}'", m.Value);
+            }
+        }
+    }
+}
