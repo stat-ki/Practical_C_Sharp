2d0e697f1cf3e91d9af5acb232e4bb8ebc3d0879
stat-ki <per_ik@outlook.com>
2020-07-24 17:50:27 +0900

    Finish 10-4

---
+++ Chapter_10/Exercise_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args[0];
+            var lines = File.ReadAllLines(file);
+            var line = lines.Select(s => Regex.Replace(s, @"\bversion=""v4.0""", @"version=""v5.0"""));
+            File.WriteAllLines(file, line);
+        }
+    }
+}
