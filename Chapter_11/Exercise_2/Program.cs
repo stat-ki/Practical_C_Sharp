2a85ea40c7752078749006a1fa67fb9b4385e68b
stat-ki <per_ik@outlook.com>
2020-07-25 16:49:40 +0900

    Finish 11-2

---
+++ Chapter_11/Exercise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "../../../kanjis.xml";
+            var xdoc = XDocument.Load(file);
+            var words = xdoc.Root.Elements().Select(x =>
+                new XElement("word",
+                    new XAttribute("kanji", x.Element("kanji").Value),
+                    new XAttribute("yomi", x.Element("yomi").Value)
+                )
+            );
+            var root = new XElement("kanjis_2", words);
+            root.Save("../../../kanjis_2.xml");
+        }
+    }
+}
