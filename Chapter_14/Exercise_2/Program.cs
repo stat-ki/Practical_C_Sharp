e64f2a765387e9cfc15ae5b56f81c6a220be8b59
stat-ki <per_ik@outlook.com>
2020-07-28 17:18:43 +0900

    Finish 14-2

---
+++ Chapter_14/Exercise_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAssemblyVersion();
+            PrintFileversion();
+        }
+
+        private static void PrintAssemblyVersion()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var ver = asm.GetName().Version;
+            Console.WriteLine("Assembly Version: {0}.{1}.{2}.{3}", ver.Major, ver.Minor, ver.Build, ver.Revision);
+        }
+
+        private static void PrintFileversion()
+        {
+            var loc = Assembly.GetExecutingAssembly().Location;
+            var ver = FileVersionInfo.GetVersionInfo(loc);
+            Console.WriteLine("File Version: {0}.{1}.{2}.{3}", ver.FileMajorPart, ver.FileMinorPart, ver.FileBuildPart, ver.FilePrivatePart);
+        }
+    }
+}
