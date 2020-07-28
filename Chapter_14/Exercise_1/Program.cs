3cd608ab554ad1d0441efc283762b30a9d0e6624
stat-ki <per_ik@outlook.com>
2020-07-28 17:05:45 +0900

    Finish 14-1

---
+++ Chapter_14/Exercise_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("../../../commands.txt");
+            ExecuteCommands(lines);
+        }
+
+        static ProcessStartInfo CreateStartupInfo (string line)
+        {
+            var items = line.Split('|');
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = items[0],
+                Arguments = items.Length >= 2 ? items[1] : null
+            };
+            return startInfo;
+        }
+
+        static void ExecuteCommands(string[] lines)
+        {
+            foreach (var line in lines)
+            {
+                var info = CreateStartupInfo(line);
+                if (string.IsNullOrWhiteSpace(info.FileName))
+                {
+                    continue;
+                }
+                using (var process = Process.Start(info))
+                {
+                    process.WaitForExit();
+                }
+            }
+        }
+    }
+}
