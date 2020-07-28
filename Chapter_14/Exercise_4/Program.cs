cc85b42ccc91183abcab35ece2d6a57df01be341
stat-ki <per_ik@outlook.com>
2020-07-28 17:44:37 +0900

    Finish 14-4

---
+++ Chapter_14/Exercise_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var html = wc.DownloadString("https://www.google.com/");
+            File.WriteAllText("../../../sample.html", html);   
+        }
+    }
+}
