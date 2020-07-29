483240769aad24b17861f7145987f841d3136692
stat-ki <per_ik@outlook.com>
2020-07-29 14:22:04 +0900

    Finish 14-6

---
+++ Chapter_14/Exercise_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var local = new DateTime(2020, 8, 19, 16, 32, 20, DateTimeKind.Local);
+            var date = new DateTimeOffset(local);
+            var utc = date.ToUniversalTime();
+            var timezone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+            var sst = TimeZoneInfo.ConvertTime(utc, timezone);
+
+            Console.WriteLine(utc);
+            Console.WriteLine(sst);
+        }
+    }
+}
