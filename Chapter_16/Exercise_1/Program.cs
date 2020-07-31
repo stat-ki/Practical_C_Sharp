029f4f4a5baa1f61e236a15af49eea85645d6912
stat-ki <per_ik@outlook.com>
2020-07-31 21:28:33 +0900

    Finish 16-1

---
+++ Chapter_16/Exercise_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync();
+            Console.ReadLine();
+        }
+
+        private static async void RunAsync()
+        {
+            var text = await TextReaderSample.ReadTextAsync("../../../oop.md");
+            Console.WriteLine(text);
+        }
+    }
+
+    static class TextReaderSample
+    {
+        public static async Task<string> ReadTextAsync(string filePath)
+        {
+            var sb = new StringBuilder();
+            var sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
+                var line = await sr.ReadLineAsync();
+                sb.AppendLine(line);
+            }
+            return sb.ToString();
+        }
+    }
+}
